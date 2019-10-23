97886ba4c8594e88655db12e57ebb8c1fc3f607e
Sarah Arnold <digipet007@gmail.com>
2019-10-23 15:59:02 -0700

    fixing sequencing issue

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
-var bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+var bodyParser = require('body-parser');
 var path = require('path');
 
 var PORT = process.env.PORT || 8080;
@@ -12,14 +13,12 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
  
 // parse some custom thing into a Buffer
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
-// app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse an HTML body into a string
 app.use(bodyParser.text({ type: 'text/html' }))
-// app.use(bodyParser.json())
 
 //include html route file, with express app parameter passed into module.exports function
-//api routes need to be included first because they pull data to display in html pages
+//NOTE: api routes need to be included first because they pull data to display in html pages
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
 
