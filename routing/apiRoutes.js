21061eb7b10e967f2126c8050cd78a08fb1700b8
Sarah Arnold <digipet007@gmail.com>
2019-10-26 10:38:01 -0700

    cleaned up api routes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -8,28 +8,10 @@ module.exports = function (app) {
         res.json(friends);
     });
 
+    //post new user data to database
     app.post('/api/friends', function (req, res) {
-        
-        // var newPerson = {
-        //         name: req.body.name,
-        //         photo: req.body.photo,
-        //         scores: [
-        //             parseInt(req.body.scores[0]),
-        //             parseInt(req.body.scores[1]),
-        //             parseInt(req.body.scores[2]),
-        //             parseInt(req.body.scores[3]),
-        //             parseInt(req.body.scores[4]),
-        //             parseInt(req.body.scores[5]),
-        //             parseInt(req.body.scores[6]),
-        //             parseInt(req.body.scores[7]),
-        //             parseInt(req.body.scores[8]),
-        //             parseInt(req.body.scores[9]),
-        //             parseInt(req.body.scores[0])
-        //         ]
-        //     }
 
-
-            console.log("post route" + req.body);
+        console.log("post route" + req.body);
 
         friends.push(req.body);
 
